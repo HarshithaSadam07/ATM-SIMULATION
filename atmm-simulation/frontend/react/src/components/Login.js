1da10fb77c5269a64c393d985b08067bb420e900
HarshithaSadam07 <harshithasadam2006@gmail.com>
2025-11-04 09:36:53 +0530

    Update Login.js

--- atmm-simulation/frontend/react/src/components/Login.js
+++ atmm-simulation/frontend/react/src/components/Login.js
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://atm-simulation.onrender.com/api';
 
 function Login({ onLogin }) {
   const [cardNumber, setCardNumber] = useState('');
